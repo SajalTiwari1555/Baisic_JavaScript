45b5abad24e6f52b27ca6446ac371a41a4e40f55
Sajal Tiwari <145692880+SajalTiwari1555@users.noreply.github.com>
2023-09-22 14:35:43 +0000

    container config done

---
+++ basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =14321;
+let accountEmail="sajaltiwari234@gmail.com"
+var accountPassword ="12345";
+accountCity ="Ghatampur";
+console.log(accountId);
+ let accountdata;
+// to use console.table to an array
+console.table([accountId,accountEmail,accountPassword,accountdata,accountCity]);
---
+++ basic/02_conversion.js
@@ -0,0 +1,35 @@
+// let score ="4554545ggggfgffgfg33";    //"345abc"
+
+let score =null;   //0
+
+// let score =undefined;  //NaN (not a number)
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+
+// console.log(typeof ValueInNumber)
+
+// console.table([ValueInNumber])
+
+
+//Notes
+// "33" => 33
+//"33abc" => Nan
+// true =>1 ,False => 0
+
+
+let IsNumber =" "   //space 
+ 
+let IsBoolean = Boolean(IsNumber)
+
+console.log(IsBoolean)
+
+
+// Notes 
+// 1 => true
+// 0 => false
+// "" => false
+// "Sajal" => true
