8b1c74c2ef90970f25b53e0a270333018390d362
Sajal Tiwari <145692880+SajalTiwari1555@users.noreply.github.com>
2023-09-27 03:43:22 +0000

    update

--- 02_Basic/Array.js
+++ 02_Basic/Array.js
@@ -1,7 +1,19 @@
 //Array
 
-const myArray =[0,3,5,7,4];
-const myHeros = ["Sajal","Sajju"];
-console.log(myArray);
-console.log(myArray[3]);
-console.log(typeof myHeros);
+// const myArray =[0,3,5,7,4];
+// myArray.push(45,67,45,89,98);
+// const myHeros = ["Sajal","Sajju"];
+// console.log(myArray);
+// console.log(myArray[3]);
+// console.log(typeof myHeros);
+
+// Array Method
+const MyArr2 =new Array(1,3,5,6,8,6);
+// MyArr2.push(8);
+// MyArr2.push(78);
+// MyArr2.push(79);
+// MyArr2.pop();
+
+// MyArr2.unshift(87);
+MyArr2.shift()
+console.log(MyArr2);
