0a81fc438a4dd4004b24431a00edac32ede7100d
Sajal Tiwari <145692880+SajalTiwari1555@users.noreply.github.com>
2023-09-28 06:52:32 +0000

    Basic done

--- 02_Basic/Array.js
+++ 02_Basic/Array.js
@@ -8,12 +8,25 @@
 // console.log(typeof myHeros);
 
 // Array Method
-const MyArr2 =new Array(1,3,5,6,8,6);
+const MyArr2 =new Array(1,2,3,4,5,6,7,8);
 // MyArr2.push(8);
 // MyArr2.push(78);
 // MyArr2.push(79);
 // MyArr2.pop();
 
 // MyArr2.unshift(87);
-MyArr2.shift()
-console.log(MyArr2);
+// MyArr2.shift()
+// console.log(MyArr2);
+
+console.log(MyArr2.includes(5));     //returns boolean value
+console.log(MyArr2.indexOf(5));       //index value returns
+
+console.log("A",MyArr2);
+const mineArr1= MyArr2.slice(1,5);
+console.log(" ",mineArr1);
+
+console.log("B",MyArr2);
+const mineArr2 = MyArr2.splice(1,5);
+console.log(mineArr2);
+console.log("C",MyArr2);
+
